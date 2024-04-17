1d9b9fd92cc5e8e018e70d819bfdd419534dbdb3
TYCamilleTinon <camille.tinon@tayloryandell.com>
2024-04-17 19:29:52 +0200

    in

---
+++ js/custom-img.js
@@ -0,0 +1,9 @@
+function photoun() {
+    const element = document.getElementById(projet-liste-1);
+    const elementdeux  = document.getElementById(project-image-1);
+    if (element) {
+      element.addEventListener('mouseenter', () => {
+        elementdeux.style.opacity = 1;
+      });
+    }
+  }
