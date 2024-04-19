bac68c5b42be54d7e77c81fd8b8f6fcc69d19d91
TYCamilleTinon <60568211+camilletinon@users.noreply.github.com>
2024-04-19 16:39:23 +0200

    project template

--- js/custom-img.js
+++ js/custom-img.js
@@ -70,3 +70,40 @@ function deloncover() {
   document.getElementById("project-image-8").style.opacity = "0";
   document.getElementById("project-image-9").style.opacity = "0";
 }
+
+
+function quinzecover() {
+  document.getElementById("project-image-1").style.opacity = "0";
+  document.getElementById("project-image-2").style.opacity = "0";
+  document.getElementById("project-image-3").style.opacity = "0";
+  document.getElementById("project-image-4").style.opacity = "0";
+  document.getElementById("project-image-5").style.opacity = "0";
+  document.getElementById("project-image-6").style.opacity = "0";
+  document.getElementById("project-image-7").style.opacity = "1";
+  document.getElementById("project-image-8").style.opacity = "0";
+  document.getElementById("project-image-9").style.opacity = "0";
+}
+
+function gin44cover() {
+  document.getElementById("project-image-1").style.opacity = "0";
+  document.getElementById("project-image-2").style.opacity = "0";
+  document.getElementById("project-image-3").style.opacity = "0";
+  document.getElementById("project-image-4").style.opacity = "0";
+  document.getElementById("project-image-5").style.opacity = "0";
+  document.getElementById("project-image-6").style.opacity = "0";
+  document.getElementById("project-image-7").style.opacity = "0";
+  document.getElementById("project-image-8").style.opacity = "1";
+  document.getElementById("project-image-9").style.opacity = "0";
+}
+
+function persocover() {
+  document.getElementById("project-image-1").style.opacity = "0";
+  document.getElementById("project-image-2").style.opacity = "0";
+  document.getElementById("project-image-3").style.opacity = "0";
+  document.getElementById("project-image-4").style.opacity = "0";
+  document.getElementById("project-image-5").style.opacity = "0";
+  document.getElementById("project-image-6").style.opacity = "0";
+  document.getElementById("project-image-7").style.opacity = "0";
+  document.getElementById("project-image-8").style.opacity = "0";
+  document.getElementById("project-image-9").style.opacity = "1";
+}
